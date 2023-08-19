8c88c655ce8436c91819b20ec336c7f70eb1e0aa
James Karnes <karnes.james@gmail.com>
2023-08-19 19:33:17 -0400

    fixing next use client error

--- pages/pdf.js
+++ pages/pdf.js
@@ -1,9 +1,10 @@
 "use client"
+
 import React, { useEffect, useState } from "react"
 import PDFFile from "../components/pdfComponent/PDFFile"
 import { PDFDownloadLink } from "@react-pdf/renderer"
 
-const pdf = () => {
+const Pdf = () => {
   const [isClient, setIsClient] = useState(false)
     useEffect(() => {
         setIsClient(true)
@@ -12,11 +13,11 @@ const pdf = () => {
     <div className="">{isClient && (
     <PDFDownloadLink document={<PDFFile name='James'/>} filename="FORM">
     {({loading}) => (loading ? <button>Loading Document...</button> : <button>Download</button> )}
-    </PDFDownloadLink>
-    {/* <PDFFile /> */})}
+    </PDFDownloadLink>)}
+    {/* <PDFFile /> */}
 
   </div>
   )
 }
 
-export default pdf
+export default Pdf
