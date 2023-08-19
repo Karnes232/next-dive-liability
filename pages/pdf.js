26fd9828284d7a37d33b0a50b23919151694040c
James Karnes <karnes.james@gmail.com>
2023-08-19 19:31:30 -0400

    test with pdf

--- pages/pdf.js
+++ pages/pdf.js
@@ -1,14 +1,20 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import PDFFile from "../components/pdfComponent/PDFFile"
 import { PDFDownloadLink } from "@react-pdf/renderer"
 
 const pdf = () => {
+  const [isClient, setIsClient] = useState(false)
+    useEffect(() => {
+        setIsClient(true)
+      }, [])
   return (
-    <div className="">
+    <div className="">{isClient && (
     <PDFDownloadLink document={<PDFFile name='James'/>} filename="FORM">
     {({loading}) => (loading ? <button>Loading Document...</button> : <button>Download</button> )}
     </PDFDownloadLink>
-    {/* <PDFFile /> */}
+    {/* <PDFFile /> */})}
+
   </div>
   )
 }
