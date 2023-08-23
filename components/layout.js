2fdc08d95c61798ca2708ee944571e25e813457e
James Karnes <karnes.james@gmail.com>
2023-08-23 09:53:25 -0400

    testing browser detect

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./headerComponent/Header"
-
+const { detect } = require('detect-browser');
 const Layout = ({ children }) => {
+  const browser = detect();
   return (
     <div className="min-h-screen font-montserrat flex flex-col justify-between overflow-x-hidden">
       <Header />
+      {browser.name}
       {children}
     </div>
   )
