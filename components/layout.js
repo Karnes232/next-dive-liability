2f90b821cd41001245e5f92472cdddcce5b894e7
James Karnes <karnes.james@gmail.com>
2023-08-23 09:58:12 -0400

    testing browser detect

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,12 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen font-montserrat flex flex-col justify-between overflow-x-hidden">
       <Header />
-      {browser.name}
+      <div className="flex flex-col justify-center items-center">
+      {browser.name}<br/>
+      {browser.version}<br/>
+      {browser.os}
+      </div>
+      
       {children}
     </div>
   )
