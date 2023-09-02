c6a4b98f672dec42f9b4055c71e35d50f4ef18a6
James Karnes <karnes.james@gmail.com>
2023-09-02 18:03:04 -0400

    added a page to find search page

--- Firebase/createLiabilityPdf.js
+++ Firebase/createLiabilityPdf.js
@@ -35,7 +35,7 @@ export default async function createLiabilityPdf(
           },
         )
       })
-    }).then(e => window.location.href = "https://dive-liability.netlify.app/")
+    })//.then(e => window.location.href = "https://dive-liability.netlify.app/")
     if (browser.name !== "ios") {
       FileSaver.saveAs(blob, fileName)
     }
