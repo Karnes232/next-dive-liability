afd648e31098809bc4aae3a1f3bb84ece83e517a
James Karnes <karnes.james@gmail.com>
2023-09-06 15:27:45 -0400

    testing pwa

--- pages/_document.js
+++ pages/_document.js
@@ -15,28 +15,28 @@ export default function Document() {
 <meta name="msapplication-TileColor" content="#2B5797" />
 <meta name="msapplication-tap-highlight" content="no" />
 <meta name="theme-color" content="#000000" />
-<link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-<link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-<link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-<link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
-<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/icons/ticon-192x192.png" />
+<link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-192x192.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 <link rel="manifest" href="/manifest.json" />
-<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+<link rel="mask-icon" href="/icons/icon-192x192.png" color="#5bbad5" />
 <link rel="shortcut icon" href="/favicon.ico" />
 
 <meta name="twitter:card" content="summary" />
-<meta name="twitter:url" content="https://yourdomain.com" />
-<meta name="twitter:title" content="PWA App" />
-<meta name="twitter:description" content="Best PWA App in the world" />
-<meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-<meta name="twitter:creator" content="@DavidWShadow" />
+<meta name="twitter:url" content="https://dive-liability.netlify.app/" />
+<meta name="twitter:title" content="Mariana Liability" />
+<meta name="twitter:description" content="Liability Papers for Mariana Caribbean Sports" />
+<meta name="twitter:image" content="https://dive-liability.netlify.app/icons/ticon-192x192.png" />
+<meta name="twitter:creator" content="@Karnes232" />
 <meta property="og:type" content="website" />
-<meta property="og:title" content="PWA App" />
-<meta property="og:description" content="Best PWA App in the world" />
-<meta property="og:site_name" content="PWA App" />
-<meta property="og:url" content="https://yourdomain.com" />
-<meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+<meta property="og:title" content="Mariana Liability" />
+<meta property="og:description" content="Liability Papers for Mariana Caribbean Sports" />
+<meta property="og:site_name" content="Mariana Liability" />
+<meta property="og:url" content="https://dive-liability.netlify.app/" />
+<meta property="og:image" content="https://dive-liability.netlify.app/icons/ticon-192x192.png" />
 
       <body>
         <Main />
