bbecac5cd3069c4c8c8efa6b4e71b9513b00ef02
James Karnes <karnes.james@gmail.com>
2023-10-02 19:20:08 -0400

    changed langauge

--- components/LanguageSelector/LanguageSelector.js
+++ components/LanguageSelector/LanguageSelector.js
@@ -18,7 +18,7 @@ const LanguageSelector = () => {
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
       >
         <option value="en">English</option>
-        <option value="es">Spanish</option>
+        <option value="es">Espanol</option>
       </select>
     </div>
   )
