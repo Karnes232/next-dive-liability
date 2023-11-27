720ec407e1e4dfe1a59c30b5bf1cbd8c3dfd5368
Karnes232 <68479200+Karnes232@users.noreply.github.com>
2023-11-27 07:58:18 -0400

    Update Header.js

--- components/headerComponent/Header.js
+++ components/headerComponent/Header.js
@@ -1,12 +1,12 @@
 import React from "react"
 import logo from "../../images/Logo-Mariana-Caribbean-Sports2.webp"
 import Image from "next/image"
-
+import logo2 from '../../images/logo.png'
 const Header = () => {
   return (
     <div className="flex justify-center items-center my-5 ">
       <Image
-        src={logo}
+        src={logo2}
         alt="Mariana Caribbean Sports"
         height="100%"
         className="object-contain h-24 md:h-28 lg:h-36"
